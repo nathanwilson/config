0cd0e7d006c042484c33ab141c4313b0c67145a8
Nathan Wilson <github.wound@nordac.simplelogin.com>
2024-06-30 13:21:31 +0100

    wip

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -75,11 +75,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  imports = [
-    ./home.nix
-    ./dev-tools.nix
-    ./browser.nix
-    ./media.nix
-    # ./utility.nix
-  ];
+  imports =
+    [ ./home.nix ./dev-tools.nix ./browser.nix ./media.nix ./utility.nix ];
 }
