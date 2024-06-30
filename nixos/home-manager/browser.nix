1ebb96c5fb6dcad466a13bf168678b312d92d85f
Nathan Wilson <github.wound@nordac.simplelogin.com>
2024-06-30 08:49:37 +0100

    feat: add firefox kagi plugin

--- nixos/home-manager/browser.nix
+++ nixos/home-manager/browser.nix
@@ -5,7 +5,13 @@
   programs.firefox = {
     enable = true;
     profiles.nathan = {
+      search.engines = {
+        kagi = {
+
+        };
+      };
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        kagi-search
         proton-pass
         privacy-badger
         ublock-origin
