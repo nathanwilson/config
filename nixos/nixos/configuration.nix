81309a5e13c31c4ef2e153c396b04f4f09b2c6cf
Nathan Wilson <github.wound@nordac.simplelogin.com>
2024-08-24 12:03:19 +0100

    feat: add nvidia config

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -53,6 +53,49 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
+  ## nvidia
+  hardware.graphics.enable = true;
+  # The same as above but for NixOS 23.11
+  #hardware.opengl = {
+  #  enable = true;
+  #  driSupport = true;
+  #};
+
+  # Load "nvidia" driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently "beta quality", so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
